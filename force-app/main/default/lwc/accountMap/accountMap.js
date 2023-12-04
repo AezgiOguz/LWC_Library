c985239c1a5bad6934fb6591ab3e32dd44a7e513
AezgiOguz <aezgioguz@gmail.com>
2023-12-04 14:31:55 +0300

    Hi

--- force-app/main/default/lwc/accountMap/accountMap.js
+++ force-app/main/default/lwc/accountMap/accountMap.js
@@ -1,18 +1,54 @@
-export default class LightningMapExample extends LightningElement {
-    mapMarkers = [
-        {
-            location: {
-                City: 'San Francisco',
-                Country: 'USA',
-                PostalCode: '94105',
-                State: 'CA',
-                Street: 'The Landmark @ One Market, Suite 300',
-            },
-            value: 'location001',
-            title: 'The Landmark Building',
-            description:
-                'The Landmark is considered to be one of the city&#39;s most architecturally distinct and historic properties', //escape the apostrophe in the string using &#39;
-            icon: 'standard:account',
-        },
+
+    import { LightningElement,api, wire } from 'lwc';
+    import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+
+
+    const FIELDS = [
+        'Account.BillingCity',
+        'Account.BillingCountry',
+        'Account.BillingPostalCode',
+        'Account.BillingState',
+        'Account.BillingStreet'
     ];
-}
+    export default class AccountMap extends LightningElement {
+        @api recordId;  
+        mapMarkers =[];
+
+        @wire(getRecord, { recordId: "$recordId", fields:FİELDS })
+        wiredAccount({ error, data }) {
+          if (data) {
+            const city=getFieldValue(data,'Account.BillingCity');
+            country=getFieldValue(data,'Account.BillingCountry');
+            postalcodet=getFieldValue(data,'Account.BillingPostalCode');
+            state=getFieldValue(data,'Account.BillingState');
+            street=getFieldValue(data,'Account.BillingStreet');
+
+          } else if (error) {
+           
+            console.log('beklenmeyen bir hata oluştu');
+          }
+        }
+        get name() {
+          return this.record.fields.Name.value;
+        }
+      }
+
+        
+       this. mapMarkers = [
+            {
+                location: {
+                    City: city,
+                    Country: country,
+                    PostalCode: postalcodet,
+                    State: state,
+                    Street: street
+                },
+                type: 'Circle',
+                radius: 200,
+                strokeColor: '#FFF000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FFF000',
+                fillOpacity: 0.35
+            },
+        ];
